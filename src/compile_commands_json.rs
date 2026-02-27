999e3dd002d81267f55559d71583045e52b1b33a
Cora <17833654450@163.com>
2026-02-27 17:36:58 +0800

    move method default of CompileCommand into trait Default

--- src/compile_commands_json.rs
+++ src/compile_commands_json.rs
@@ -4,7 +4,8 @@ use serde::{Deserialize, Serialize};
 
 use crate::{cli::arg::get_args, cxon::get_cxon_config, object::source::Source};
 
-static COMPILE_COMMANDS_LIST: LazyLock<Mutex<Vec<CompileCommand>>> = LazyLock::new(|| Mutex::new(Vec::new()));
+static COMPILE_COMMANDS_LIST: LazyLock<Mutex<Vec<CompileCommand>>> =
+    LazyLock::new(|| Mutex::new(Vec::new()));
 
 pub fn add_compile_command(command: CompileCommand) {
     COMPILE_COMMANDS_LIST.lock().unwrap().push(command);
@@ -14,14 +15,19 @@ pub fn generate_compile_commands_json() -> Result<(), Box<dyn std::error::Error>
     let commands = COMPILE_COMMANDS_LIST.lock().unwrap().clone();
     let compile_commands_json = serde_json::to_string_pretty(&commands)?;
 
-    let path = match get_cxon_config().read().unwrap().export_compile_commands_path.clone() {
+    let path = match get_cxon_config()
+        .read()
+        .unwrap()
+        .export_compile_commands_path
+        .clone()
+    {
         Some(mut path) => {
             if !path.ends_with("compile_commands.json") {
                 path = path.join("compile_commands.json");
             }
 
             path
-        },
+        }
         // default path is `build_dir`/compile_commands.json
         None => get_cxon_config()
             .read()
@@ -44,17 +50,20 @@ pub struct CompileCommand {
 }
 
 impl CompileCommand {
-    pub fn default() -> Self {
+    pub fn from_source(source: Source) -> Self {
+        let mut command = Self::default();
+        command.file = source.get_path().to_string_lossy().to_string();
+        command
+    }
+}
+
+
+impl Default for CompileCommand {
+    fn default() -> Self {
         Self {
             directory: get_args().project_dir.to_string_lossy().to_string(),
             command: String::new(),
             file: String::new(),
         }
-    }
-
-    pub fn from_source(source: Source) -> Self {
-        let mut command = Self::default();
-        command.file = source.get_path().to_string_lossy().to_string();
-        command
     }
 }
